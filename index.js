13a50e2e4578573bc245cdc932c593bb153de7a9
shinar3@gmail.com <shenarfichman@Siddeeqahs-MacBook-Pro.local>
2022-08-23 13:06:54 -0400

    commit tree 61172c0d315a4471c628a8f09a40ab4ddd81f4cd

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+ cats.push(name);
+}
+function destructivelyPrependCat(name){
+cats.unshift(name);
+}
+
+function  destructivelyRemoveLastCat(){
+cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+ cats.shift();  
+}
+
+function appendCat(name){
+
+const copyOfcats = [...cats, name];
+
+return copyOfcats
+
+}
+function prependCat(name){
+
+    const copyOfcats = [name, ...cats];
+    
+    return copyOfcats
+    
+    }
+
+    function removeLastCat(){
+
+       
+        const copyOfcats = cats.slice(0, cats.length -1);
+    
+        return copyOfcats;
+    }
+    function removeFirstCat(){
+
+       
+        const copyOfcats = cats.slice(1);
+    
+        return copyOfcats;
+    }
